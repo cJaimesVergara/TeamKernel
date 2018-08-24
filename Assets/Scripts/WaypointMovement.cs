303e38b9e4fac8695c4aeab982e1a320646685b3
Carlos Jaimes <cjaimes_vergara@hotmail.com>
2018-08-23 19:26:48 -0500

    Waypoint logic added. TODO Check scale, everything looks so small

---
+++ Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointMovement : MonoBehaviour {
+	
+	public GameObject player;
+
+
+	public float height = 2;
+	public bool teleport = true;
+
+	public float maxMoveDistance = 10;
+	private bool moving = false;
+
+
+
+	public void Move(GameObject waypoint) {
+		if (!teleport) {
+			iTween.MoveTo (player, 
+				iTween.Hash (
+					"position", new Vector3 (waypoint.transform.position.x, waypoint.transform.position.y + height / 2, waypoint.transform.position.z), 
+					"time", .2F, 
+					"easetype", "linear"
+				)
+			);
+		} else {
+			player.transform.position = new Vector3 (waypoint.transform.position.x, 
+                waypoint.transform.position.y + height / 2, 
+                waypoint.transform.position.z);
+		}
+	}
+
+}
+
