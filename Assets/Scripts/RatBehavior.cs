ad32dcf79d4c66b8dd5d56770e9a162703ba2d0f
Carlos\Carlos Jaimes <cjaimes_vergara@hotmail.com>
2018-08-28 00:55:20 -0500

    Click on rat returns it to the cage.

---
+++ Assets/Scripts/RatBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatBehavior : MonoBehaviour {
+
+    public GameObject putInObject;
+    public GameObject ratInCage;
+
+
+    public void ToCage() {
+        ratInCage.SetActive(true);
+        GameObject.Destroy(this.gameObject);
+    }
+
+    public void makenoise() {
+        Debug.Log("NOISE");
+    }
+	
+}
