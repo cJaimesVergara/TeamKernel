1ff757137fc232435c4780640ab19bf472d06c86
DESKTOP-CLGEOEL\CarlosJaimes <cjaimes_vergara@hotmail.com>
2018-08-30 07:58:43 -0500

    Test Tube Prefab fixed. Test Tube behavior added.

---
+++ Assets/TestTube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTube : MonoBehaviour {
+
+    public GameObject putInObject;
+    public Vector3 position;
+    public Vector3 rotation;
+
+    public void PutInObject()
+    {
+        this.transform.SetParent(putInObject.transform);
+        this.transform.eulerAngles = rotation;
+        this.transform.localPosition = position;
+    }
+    
+
+}
